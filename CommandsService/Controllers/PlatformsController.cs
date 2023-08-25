a6f21ec0edc5380685c6e8e88b3433b11d3b8774
soleshhaker <tobi1560@t.pl>
2023-08-25 12:40:22 +0200

    CommandsSerive repo and controllers

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsSerivce.Controllers
@@ -6,9 +9,22 @@ namespace CommandsSerivce.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandService");
             
+            var platformItems = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
 
         [HttpPost]
---
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+
+namespace CommandsService.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+        }
+    }
+}
